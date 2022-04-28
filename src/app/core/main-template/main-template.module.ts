bb6cff994702876815503e14ac47b4aa76800fb9
Julio Rossell <juliorosselltapia@gmail.com>
2022-04-28 12:30:30 -0500

    feat: remove sidebar not used

--- src/app/core/main-template/main-template.module.ts
+++ src/app/core/main-template/main-template.module.ts
@@ -4,14 +4,12 @@ import { CommonModule } from '@angular/common';
 import { MainTemplateComponent } from './main-template.component';
 import { FooterModule } from '../footer/footer.module';
 import { HeaderModule } from '../header/header.module';
-import { SidebarModule } from '../sidebar/sidebar.module';
 import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     FooterModule,
     HeaderModule,
-    SidebarModule,
     CommonModule,
     RouterModule,
   ],
